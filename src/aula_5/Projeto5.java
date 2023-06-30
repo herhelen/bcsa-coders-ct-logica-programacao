decf71a96e6e8a5758544fb5343b5cb68ce5022e
Helen Her <her.helen@gmail.com>
2023-06-30 20:56:10 -0300

    trocando tab por espacos

--- src/aula_5/Projeto5.java
+++ src/aula_5/Projeto5.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Projeto5 {
 
     public static double calculaDescontoINSS(double salarioBruto) {
-        double porcentagemDesconto = 0;
         /*
-        * Salário	                % Desconto INSS
-        * até 1.212,00	            7,5%
-        * de 1212,01 até 2.427,35	9%
-        * de 2.427,36 até 3.641,03	12%
-        * de 3.641,04 até 7.087,22	14%
-        * */
+         * Salário                   % Desconto INSS
+         * até 1.212,00              7,5%
+         * de 1212,01 até 2.427,35   9%
+         * de 2.427,36 até 3.641,03  12%
+         * de 3.641,04 até 7.087,22  14%
+         * */
+        double porcentagemDesconto = 0;
 
         if(salarioBruto <= 1212.00) {
             porcentagemDesconto = 0.075; // 7,5%
@@ -30,16 +30,15 @@ public class Projeto5 {
     }
 
     public static double calculaDescontoIR(double salarioBruto) {
-        double porcentagemDesconto = 0;
-
         /*
-         * Salário	                % Desconto Imposto de Renda
-         * até 1.903,98	            0%
-         * de 1.903,99 até 2.826,65	7,5%
-         * de 2.826,66 até 3.751,05	15%
-         * de 3.751,06 até 4.664,68	22,50%
-         * Acima de 4.664,68	    27,50%
+         * Salário                  % Desconto Imposto de Renda
+         * até 1.903,98             0%
+         * de 1.903,99 até 2.826,65 7,5%
+         * de 2.826,66 até 3.751,05 15%
+         * de 3.751,06 até 4.664,68 22,50%
+         * Acima de 4.664,68        27,50%
          * */
+        double porcentagemDesconto = 0;
 
         if(salarioBruto <= 1903.98) {
             porcentagemDesconto = 0; // 0%
