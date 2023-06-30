ced5d546db3a5bce11363bd025ee8f45c0c905b6
Helen Her <her.helen@gmail.com>
2023-06-30 18:22:23 -0300

    2023.06.26 aula 1

---
+++ src/aula_1/Aula1.java
@@ -0,0 +1,20 @@
+package aula_1;
+
+public class Aula1 {
+    public static void main(String[] args) {
+
+        // Aula 1
+        String nome = "Vitor";
+        int idade = 26;
+        double altura = 1.76;
+        System.out.println("nome = " + nome + " idade = " + idade + " altura = " + altura);
+        System.out.printf("nome: %s%nidade: %d%naltura: %.1f", nome, idade, altura);
+
+        // Operadores aritmeticos
+        int numA = 10;
+        int numB = 5;
+        int res = numA + numB;
+        System.out.println(res);
+
+    }
+}
