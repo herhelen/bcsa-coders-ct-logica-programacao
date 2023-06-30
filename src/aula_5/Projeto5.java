61640b8123d41a6a8296b48f708467cb21337ef1
Helen Her <her.helen@gmail.com>
2023-06-30 19:17:27 -0300

    2023.06.30 avaliacao projetp - calculo de salario

---
+++ src/aula_5/Projeto5.java
@@ -0,0 +1,16 @@
+package aula_5;
+
+import java.util.Scanner;
+
+public class Projeto5 {
+
+    public static void main(String[] args) {
+
+        /**
+         * Cálculo de Salário
+         */
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
